46b2ec6755a9146c274d9e3679525cb63c3ffdcd
alyx <alexanderbek3@gmail.com>
2024-06-09 17:51:59 +0930

    example 1, annoying long urls

---
+++ src/example1.py
@@ -0,0 +1,15 @@
+import configparser
+import json
+import requests
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+API_KEY = config["API"]["KEY"]
+
+if __name__ == "__main__":
+    headers = {"x-apikey": API_KEY}
+
+    request = requests.get(
+        "https://www.virustotal.com/api/v3/ip_addresses/1.1.1.1", headers=headers
+    )
+    print(json.dumps(request.json(), indent=2))
